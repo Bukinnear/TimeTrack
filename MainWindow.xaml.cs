247e2e93a2140ac22c9a44fd0876442cef830bd9
Jared <Bukinnear@gmail.com>
2020-11-03 18:42:12 +1100

    Fixes to Export to CSV function

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -63,6 +63,7 @@ namespace TimeTrack
             {
                 DgTimeRecords.SelectedIndex = DgTimeRecords.SelectedIndex + 1;
                 DgTimeRecords.Focus();
+                ExportToCSV(CSVName());
             }
         }
 
