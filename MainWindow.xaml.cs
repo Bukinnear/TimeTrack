6a3060dc815b8482cc8be00584768d9c840a5040
Jared Kinnear <bukinnear@gmail.com>
2022-07-29 11:03:02 +1000

    Removing unused "using"s

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,31 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 using System.Runtime.CompilerServices;
-using System.Linq.Expressions;
 
 using CsvHelper;
-using CsvHelper.Configuration;
-using System.Runtime.InteropServices.WindowsRuntime;
-using CsvHelper.Configuration.Attributes;
-using System.Runtime.InteropServices;
 
 namespace TimeTrack
 {
